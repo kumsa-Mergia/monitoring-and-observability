33197299387e4b44c033422811ff308faef6719a
kumsa-Mergia <kumsamergia@gmail.com>
2025-01-28 14:40:36 +0300

    Deploy Prometheus and Grafana container

--- deployment/main.tf
+++ deployment/main.tf
@@ -11,6 +11,8 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+# Define Grafana container
+
 resource "docker_image" "grafana" {
   name = "grafana/grafana:latest"
 }
@@ -29,4 +31,22 @@ resource "docker_container" "grafana" {
     "GF_SECURITY_ADMIN_PASSWORD=kumsa@1234",    # Set Grafana admin password
   ]
 }
+# Define Prometheus container
 
+resource "docker_image" "prometheus" {
+  name = "prom/prometheus:latest"
+}
+
+resource "docker_container" "prometheus" {
+  image = docker_image.prometheus.name
+  name  = "prometheus"
+  ports {
+    internal = 9090
+    external = 9090
+  }
+  restart = "always"
+
+  env = [
+    "PROMETHEUS_STORAGE_PATH=/prometheus",  # Example environment variable for Prometheus
+  ]
+}
